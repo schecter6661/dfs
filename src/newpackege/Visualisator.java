445169adc1359cf39eeea013c0eb2b11b20df08c
daria <xarqwed6661@gmail.com>
2016-06-30 11:55:53 +0300

    added dfs visualisation, exceptions

--- src/newpackege/Visualisator.java
+++ src/newpackege/Visualisator.java
@@ -1,142 +1,155 @@
 package newpackege;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.view.mxGraph;
+import javafx.util.Pair;
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 
-public class Visualisator extends JFrame implements Depht {
+public class Visualisator extends JPanel implements Depht {
 
-    private static final long serialVersionUID = -2707712944901661771L;
+    private int n; //количество вершин в графе
+    private int m; //количество дуг в графе
+    private ArrayList<ArrayList<Integer>> adj; //список смежности
+    private boolean used[]; //массив для хранения информации о пройденных и не пройденных вершинах
+    private ArrayList<Integer> listDepht;
+    private ArrayList<Pair<Integer, Integer>> listEdges;
 
-    private int n; //РєРѕР»РёС‡РµСЃС‚РІРѕ РІРµСЂС€РёРЅ РІ РѕСЂРіСЂР°С„Рµ
-    private int m; //РєРѕР»РёС‡РµСЃС‚РІРѕРµ РґСѓРі РІ РѕСЂРіСЂР°С„Рµ
-    //  int v;
-    private ArrayList<Integer> adj[]; //СЃРїРёСЃРѕРє СЃРјРµР¶РЅРѕСЃС‚Рё
-    private boolean used[]; //РјР°СЃСЃРёРІ РґР»СЏ С…СЂР°РЅРµРЅРёСЏ РёРЅС„РѕСЂРјР°С†РёРё Рѕ РїСЂРѕР№РґРµРЅРЅС‹С… Рё РЅРµ РїСЂРѕР№РґРµРЅРЅС‹С… РІРµСЂС€РёРЅР°С…
-
-    private BufferedReader cin;
     private PrintWriter cout;
-    private StringTokenizer tokenizer;
 
     @SuppressWarnings("unused")
     public Visualisator() throws IOException {
-        super("Visualisation");
+        this.setSize(500, 500);
+        listDepht = new ArrayList<>();
+        listEdges = new ArrayList<>();
     }
 
-    public void pridumaySama(){
-
+    public void functionVisual() {
         mxGraph graph = new mxGraph();
         Object parent = graph.getDefaultParent();
-
         graph.getModel().beginUpdate();
-        
-
-        // readData();
-        // int n; // РљРѕР»РёС‡РµСЃС‚РІРѕ РІРµСЂС€РёРЅ
         Object points[] = new Object[n];
-
         double phi0 = 0;
         double phi = 2 * Math.PI / n;
-        int r = 250; // СЂР°РґРёСѓСЃ РѕРєСЂСѓР¶РЅРѕСЃС‚Рё
+        int r = 250; // радиус окружности
         for (int i = 0; i < points.length; i++) {
             points[i] = graph.insertVertex(parent, null, i + 1, 300 + r * Math.cos(phi0), 300 + r * Math.sin(phi0), 18, 18, "shape=ellipse");
             phi0 += phi;
         }
-// РЈР¶ СЂР°Р·Р±РµСЂРёСЃСЊ СЃ СЌС‚РёРј СЃР°РјР°, Рё РїСЂРѕСЃС‚Рѕ РїРѕРґСѓРјР°Р№, РєР°Рє С‚С‹ С…РѕС‚РµР»Р° СЃРѕРµРґРёРЅСЏС‚СЊ С‚РѕС‡РєРё, РµСЃР»Рё С‚С‹ РёС… РЅРµ РЅР°СЂРёСЃРѕРІР°Р»Р°. Р”Р»СЏ СЌС‚РѕРіРѕ СЃСЂР°РІРЅРё, С‡С‚Рѕ РЅР°РїРёСЃР°РЅРѕ С‚СѓС‚ Рё С‚Рѕ, С‡С‚Рѕ РЅР°РїРёСЃР°РЅРѕ Сѓ С‚РµР±СЏ.
-//        for (int i = 0; i < points.length; i++) {
-//
-//            for (int j = 0; j < points.length; j++) {
-//                if (adj[i].get(i) == j) {
-//                    graph.insertEdge(parent, null, null, points[i], points[j]);
-//
-//                }
-//
-//            }
-//        }
-
-
+        for (int i = 0; i < adj.size(); i++) {
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                graph.insertEdge(parent, null, null, points[i], points[adj.get(i).get(j)]);
+            }
+        }
         graph.getModel().endUpdate();
-
         mxGraphComponent graphComponent = new mxGraphComponent(graph);
-        getContentPane().add(graphComponent);
+        this.add(graphComponent);
+        this.revalidate();
+
     }
+
     @Override
     public void dfs(int v) {
-        //РµСЃР»Рё РІРµСЂС€РёРЅР° СЏРІР»СЏРµС‚СЃСЏ РїСЂРѕР№РґРµРЅРЅРѕР№, С‚Рѕ РЅРµ РїСЂРѕРёР·РІРѕРґРёРј РёР· РЅРµРµ РІС‹Р·РѕРІ РїСЂРѕС†РµРґСѓСЂС‹
-        if (used[v]) {
-            return;
+        //если вершина является пройденной, то не производим из нее вызов процедуры
+        used[v] = true; //помечаем вершину как пройденную
+        listDepht.add(v + 1);
+        //запускаем обход из всех вершин, смежных с вершиной v
+        for (int i = 0; i < adj.get(v).size(); ++i) {
+            int w = adj.get(v).get(i);
+            if (!used[w]) {
+                listEdges.add(new Pair(v, w));
+                dfs(w); //вызов обхода в глубину от вершины w, смежной с вершиной v
+            }
         }
-        used[v] = true; //РїРѕРјРµС‡Р°РµРј РІРµСЂС€РёРЅСѓ РєР°Рє РїСЂРѕР№РґРµРЅРЅСѓСЋ
-        cout.print((v + 1) + " ");
-        //Р·Р°РїСѓСЃРєР°РµРј РѕР±С…РѕРґ РёР· РІСЃРµС… РІРµСЂС€РёРЅ, СЃРјРµР¶РЅС‹С… СЃ РІРµСЂС€РёРЅРѕР№ v
-        for (int i = 0; i < adj[v].size(); ++i) {
-            int w = adj[v].get(i);
-            dfs(w); //РІС‹Р·РѕРІ РѕР±С…РѕРґР° РІ РіР»СѓР±РёРЅСѓ РѕС‚ РІРµСЂС€РёРЅС‹ w, СЃРјРµР¶РЅРѕР№ СЃ РІРµСЂС€РёРЅРѕР№ v
+    }
+
+
+    public void dfsVisual() {
+        mxGraph graph = new mxGraph();
+        Object parent = graph.getDefaultParent();
+        graph.getModel().beginUpdate();
+        Object points[] = new Object[n];
+        double phi0 = 0;
+        double phi = 2 * Math.PI / n;
+        int r = 250; // радиус окружности
+        for (int i = 0; i < points.length; i++) {
+            points[i] = graph.insertVertex(parent, null, i + 1, 300 + r * Math.cos(phi0), 300 + r * Math.sin(phi0), 18, 18, "shape=ellipse");
+            phi0 += phi;
         }
+        for (int i = 0; i < listEdges.size(); i++) {
+            graph.insertEdge(parent, null, null, points[listEdges.get(i).getKey()], points[listEdges.get(i).getValue()]);
+        }
+        graph.getModel().endUpdate();
+        mxGraphComponent graphComponent = new mxGraphComponent(graph);
+        this.add(graphComponent);
+        this.revalidate();
 
     }
 
     @Override
     public void readData() throws IOException {
-        cin = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter number of vertices and edges: ");
+        Scanner cin = new Scanner(System.in);
         cout = new PrintWriter(System.out);
-        tokenizer = new StringTokenizer(cin.readLine());
 
-        n = Integer.parseInt(tokenizer.nextToken()); //СЃС‡РёС‚С‹РІР°РµРј РєРѕР»РёС‡РµСЃС‚РІРѕ РІРµСЂС€РёРЅ РіСЂР°С„Р°
-        m = Integer.parseInt(tokenizer.nextToken()); //СЃС‡РёС‚С‹РІР°РµРј РєРѕР»РёС‡РµСЃС‚РІРѕ СЂРµР±РµСЂ РіСЂР°С„Р°
+        n = cin.nextInt(); //считываем количество вершин графа
+        m = cin.nextInt(); //считываем количество ребер графа
+        System.out.print("Enter vertices:\n");
+        //инициализируем список смежности графа размерности n
 
-        //РёРЅРёС†РёР°Р»РёР·РёСЂСѓРµРј СЃРїРёСЃРѕРє СЃРјРµР¶РЅРѕСЃС‚Рё РіСЂР°С„Р° СЂР°Р·РјРµСЂРЅРѕСЃС‚Рё n
-        adj = new ArrayList[n];
+        adj = new ArrayList<ArrayList<Integer>>(n);
         used = new boolean[n];
 
-        for (int i = 0; i < n; ++i) {
-            adj[i] = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<Integer>());
         }
-
-        //СЃС‡РёС‚С‹РІР°РµРј РіСЂР°С„, Р·Р°РґР°РЅРЅС‹Р№ СЃРїРёСЃРєРѕРј СЂРµР±РµСЂ
-        for (int i = 0; i < m; ++i) {
-            tokenizer = new StringTokenizer(cin.readLine());
-            int v = Integer.parseInt(tokenizer.nextToken()); //vertex 1
-            int w = Integer.parseInt(tokenizer.nextToken()); //vertex 2
+        //считываем граф, заданный списком ребер
+        for (int i = 0; i < m; i++) {
+            int v = cin.nextInt(); //vertex 1
+            int w = cin.nextInt(); //vertex 2
             v--;
             w--;
-            adj[v].add(w);
-            adj[w].add(v);
-
+            adj.get(v).add(w);
+            adj.get(w).add(v);
         }
-
-
     }
 
     @Override
     public void run() throws IOException {
         readData();
         for (int v = 0; v < n; ++v) {
-            dfs(v);
+            if (!used[v]) {
+                dfs(v);
+            }
         }
-        // visualisation();
-
     }
 
 
     public static void main(String[] args) throws IOException {
-
-        Visualisator frame = new Visualisator();
-        frame.run();
-        frame.pridumaySama();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(700, 700);
+        JFrame frame = new JFrame();
+        frame.setSize(800, 800);
+        Visualisator panel = new Visualisator();
+        try {
+            panel.run();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            panel.functionVisual();
+        } catch (RuntimeException e1) {
+            System.out.println(e1.getMessage());
+        }
+        panel.dfsVisual();
+        frame.add(panel);
         frame.setVisible(true);
     }
-
 }
 
