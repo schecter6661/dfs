69c20d1d0fd2a59dcf9ca6f263ef1c2e9c69c944
daria <xarqwed6661@gmail.com>
2016-06-30 18:48:20 +0300

    classes separated -> Algo.java

--- src/newpackege/Depht.java
+++
@@ -1,9 +0,0 @@
-package newpackege;
-
-import java.io.IOException;
-
-public interface Depht {
-     void dfs(int v);
-     void readData() throws IOException;
-     void run() throws IOException;
-}
--- src/newpackege/Visualisator.java
+++ src/newpackege/Visualisator.java
@@ -1,27 +1,26 @@
 package newpackege;
 
 import javax.swing.*;
+
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.view.mxGraph;
 import javafx.util.Pair;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Scanner;
 
+public class Visualisator extends JPanel {
 
-public class Visualisator extends JPanel implements Depht {
+    public static int n; //количество вершин в графе
+    public static int m; //количество дуг в графе
+    public ArrayList<ArrayList<Integer>> adj; //список смежности
+    public boolean used[]; //массив для хранения информации о пройденных и не пройденных вершинах
+    public ArrayList<Integer> listDepht;
+    public ArrayList<Pair<Integer, Integer>> listEdges;
 
-    private static int n; //количество вершин в графе
-    private static int m; //количество дуг в графе
-    private ArrayList<ArrayList<Integer>> adj; //список смежности
-    private boolean used[]; //массив для хранения информации о пройденных и не пройденных вершинах
-    private ArrayList<Integer> listDepht;
-    private ArrayList<Pair<Integer, Integer>> listEdges;
+    public PrintWriter cout;
 
-    private PrintWriter cout;
 
     @SuppressWarnings("unused")
     public Visualisator() throws IOException {
@@ -54,21 +53,6 @@ public class Visualisator extends JPanel implements Depht {
     }
 
 
-
-   /* public void dfs(int v) {
-        //если вершина является пройденной, то не производим из нее вызов процедуры
-        used[v] = true; //помечаем вершину как пройденную
-        listDepht.add(v + 1);
-        //запускаем обход из всех вершин, смежных с вершиной v
-        for (int i = 0; i < adj.get(v).size(); ++i) {
-            int w = adj.get(v).get(i);
-            if (!used[w]) {
-                listEdges.add(new Pair(v, w));
-                dfs(w); //вызов обхода в глубину от вершины w, смежной с вершиной v
-            }
-        }
-    }*/
-
     public void dfsVisual() {
         mxGraph graph = new mxGraph();
         Object parent = graph.getDefaultParent();
@@ -91,50 +75,23 @@ public class Visualisator extends JPanel implements Depht {
         this.revalidate();
     }
 
-    @Override
-    public void dfs(int v) {
 
-    }
-
-    @Override
-    public void readData() throws IOException {
-        System.out.print("Enter number of vertices and edges: ");
-        Scanner cin = new Scanner(System.in);
-        cout = new PrintWriter(System.out);
-
-        n = cin.nextInt(); //считываем количество вершин графа
-        m = cin.nextInt(); //считываем количество ребер графа
-        System.out.print("Enter vertices:\n");
-        //инициализируем список смежности графа размерности n
-
-        adj = new ArrayList<ArrayList<Integer>>(n);
-        used = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<Integer>());
+    public static void main(String[] args) throws IOException {
+        JFrame frame = new JFrame();
+        frame.setSize(800, 800);
+        Algo panel = new Algo();
+        try {
+            panel.run();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
         }
-        //считываем граф, заданный списком ребер
-        for (int i = 0; i < m; i++) {
-            int v = cin.nextInt(); //vertex 1
-            int w = cin.nextInt(); //vertex 2
-            v--;
-            w--;
-            adj.get(v).add(w);
-            adj.get(w).add(v);
+        try {
+            panel.functionVisual();
+        } catch (RuntimeException e1) {
+            System.out.println(e1.getMessage());
         }
+        panel.dfsVisual();
+        frame.add(panel);
+        frame.setVisible(true);
     }
-
-    @Override
-    public void run() throws IOException {
-        readData();
-        for (int v = 0; v < n; ++v) {
-            if (!used[v]) {
-                dfs(v);
-            }
-        }
-    }
-
-
-
 }
-
